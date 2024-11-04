a987c8f217d3bdc9d251bbcf7eaa4889ec68b085
NguyenVietSon45 <nguyenvietson000@gmail.com>
2024-11-04 18:02:04 +0700

    change

--- MyReadingApp/app/build.gradle.kts
+++ MyReadingApp/app/build.gradle.kts
@@ -7,6 +7,9 @@ plugins {
 android {
     namespace = "com.example.myreadingapp"
     compileSdk = 34
+    buildFeatures {
+        viewBinding = true
+    }
 
     defaultConfig {
         applicationId = "com.example.myreadingapp"
@@ -36,6 +39,17 @@ android {
 dependencies {
 
     implementation(libs.appcompat)
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation("com.squareup.picasso:picasso:2.71828")
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation("androidx.appcompat:appcompat:1.6.1");
+    implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-storage:19.2.2")
+    implementation ("com.github.dhaval2404:imagepicker:2.1")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
